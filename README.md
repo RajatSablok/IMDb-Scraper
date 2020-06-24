7f5422ed9340b824b38afdee947a25dd3215a1f2
RajatSablok <rajat.main06@gmail.com>
2020-06-25 00:10:31 +0530

    readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# IMDb-Scraper
+# IMDb Scraper
+
+> <Subtitle>
+> A simple web app to scrape data off of IMDb.
+
+---
+
+<!-- [![DOCS](https://img.shields.io/badge/Documentation-see%20docs-green?style=flat-square&logo=appveyor)](https://documenter.getpostman.com/view/8339014/Szzheyjd) -->
+
+<!-- [![UI ](https://img.shields.io/badge/User%20Interface-Link%20to%20UI-orange?style=flat-square&logo=appveyor)](INSERT_UI_LINK_HERE) -->
+<!-- ## Features
+- Directly save y
+- Endpoint for all categories
+- Endpoint for both English and Hindi news
+- Directly save your results to a CSV file -->
+
+## Instructions to run
+
+```
+$ git clone https://github.com/RajatSablok/IMDb-Scraper.git
+$ cd IMDb-Scraper
+$ npm install
+$ npm start
+```
+
+## Contributors
+
+- <a href="https://github.com/RajatSablok">Rajat Sablok</a>
+
+## License
+
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-const request = require('request-promise')
-const cheerio = require('cheerio')
-const fs = require('fs')
-const json2csv = require('json2csv').Parser
+const request = require("request-promise");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const json2csv = require("json2csv").Parser;
 
 const movieURL = "https://www.imdb.com/title/tt0816692/?ref_=tt_sims_tt";
 
-(
-    async() => {
-    const imdbData = []
-    
-        const response = await request({
-            uri : movieURL,
-            headers : {
-                accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-encoding": "gzip, deflate, br",
-                "accept-language": "en-GB,en-US;q=0.9,en;q=0.8"
-            },
-            gzip : true
-        })
-        
-        let $ = cheerio.load(response)
-        let title = $('div[class="title_wrapper"] > h1').text().trim()
-        let rating = $('div[class="ratingValue"] > strong > span').text()
-        let runTime = $('div[class="subtext"] > time').text().trim()
-        let releaseDate = $('a[title="See more release dates"]').text().trim()
-        let summary = $('div[class="summary_text"]').text().trim()
-    
-        imdbData.push({
-            title,
-            rating,
-            runTime,
-            releaseDate,
-            summary
-        });
-    
-    const j2cp = new json2csv()
-    const csv = j2cp.parse(imdbData)
-    
-    fs.writeFileSync("./imdb.csv", csv, "utf-8")
+(async () => {
+  const imdbData = [];
 
-})()
+  const response = await request({
+    uri: movieURL,
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+    },
+    gzip: true,
+  });
+
+  let $ = cheerio.load(response);
+  let title = $('div[class="title_wrapper"] > h1').text().trim();
+  let rating = $('div[class="ratingValue"] > strong > span').text();
+  let runTime = $('div[class="subtext"] > time').text().trim();
+  let releaseDate = $('a[title="See more release dates"]').text().trim();
+  let summary = $('div[class="summary_text"]').text().trim();
+
+  imdbData.push({
+    title,
+    rating,
+    runTime,
+    releaseDate,
+    summary,
+  });
+
+  const j2cp = new json2csv();
+  const csv = j2cp.parse(imdbData);
+
+  fs.writeFileSync("./imdb.csv", csv, "utf-8");
+})();
